c32e493d6e1945b071ad6b710ad3ec03ba749044
Just A Mouse <colin-mercer@hotmail.co.uk>
2023-04-27 17:22:35 +0100

    Update hooker_db.py
    Ensure table names don't start with a number

--- modules/hooker_db.py
+++ modules/hooker_db.py
@@ -5,15 +5,16 @@ class DB_object():
     def __init__(self):
         self.db_name = "Discord_Image_Hooker_SQLite.db"
 
+
     async def make_table(self, table_name):
-        create_table_query = "CREATE TABLE IF NOT EXISTS %s (md5 CHARACTER(32) PRIMARY KEY);" % table_name
+        create_table_query = "CREATE TABLE IF NOT EXISTS %s (md5 CHARACTER(32) PRIMARY KEY);" % await self.fix_start_with_numbers(table_name)
         async with aiosqlite.connect(self.db_name) as db:
             await db.execute(create_table_query)
             await db.commit()
 
     async def check_for_value(self, table_name, md5):
         # use TOP to terminate loop after finding first record
-        check_table_query = "SELECT EXISTS (SELECT md5 FROM %r WHERE md5 = %r) LIMIT 1" % (table_name, md5)
+        check_table_query = "SELECT EXISTS (SELECT md5 FROM %r WHERE md5 = %r) LIMIT 1" % (await self.fix_start_with_numbers(table_name), md5)
         async with aiosqlite.connect(self.db_name) as db:
             cursor = await db.execute(check_table_query)
             total_number_of_rows = await cursor.fetchone()
@@ -22,7 +23,7 @@ class DB_object():
         return total_number_of_rows
 
     async def add_md5_checksum(self, table_name, md5):
-        md5_insert_query = "INSERT INTO %r (md5) VALUES (%r)" % (table_name, md5)
+        md5_insert_query = "INSERT INTO %r (md5) VALUES (%r)" % (await self.fix_start_with_numbers(table_name), md5)
         async with aiosqlite.connect(self.db_name) as db:
             await db.execute(md5_insert_query)
             await db.commit()
@@ -33,3 +34,10 @@ class DB_object():
         for o in offenders:
             name = str(name).replace(o, '')
         return name
+
+    async def fix_start_with_numbers(self, item):
+        # table names can't start with a number
+        if item[0].isdigit():
+            return "a" + item
+        else:
+            return item
